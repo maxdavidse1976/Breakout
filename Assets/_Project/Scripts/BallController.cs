2499d66931017021d127f3999090e2b4a6bcb618
Max Davidse <m.davidse@rubicon.nl>
2023-10-09 15:16:39 +0200

    Hooked up the scoring and losing of lives

--- Assets/_Project/Scripts/BallController.cs
+++ Assets/_Project/Scripts/BallController.cs
@@ -1,10 +1,15 @@
 using UnityEngine;
+using TMPro;
 
 public class BallController : MonoBehaviour
 {
     [SerializeField] Rigidbody2D _rigidbody;
     [SerializeField] float _ballSpeed = 4f;
     [SerializeField] float _bottomBounds = -4.8f;
+    [SerializeField] float _score = 0;
+    [SerializeField] int _lives = 5;
+    [SerializeField] TMP_Text _scoreText;
+    [SerializeField] GameObject[] _livesImage;
 
     void Start()
     {
@@ -16,6 +21,8 @@ public class BallController : MonoBehaviour
     {
         if (transform.position.y < _bottomBounds || Input.GetKeyDown(KeyCode.R))
         {
+            _lives--;
+            _livesImage[_lives].SetActive(false);
             ResetBall();
         }
     }
@@ -36,6 +43,9 @@ public class BallController : MonoBehaviour
         if (collision.gameObject.CompareTag("Brick"))
         {
             Destroy(collision.gameObject);
+
+            _score += 10;
+            _scoreText.text = _score.ToString("00000");
         }
     }
 }
