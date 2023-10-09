ea428d740257d2dc6f424b5f238600d1e708109a
Max Davidse <m.davidse@rubicon.nl>
2023-10-09 09:44:18 +0200

    Added bounds that the player can't cross

--- Assets/_Project/Scripts/PaddleController.cs
+++ Assets/_Project/Scripts/PaddleController.cs
@@ -3,11 +3,17 @@ using UnityEngine;
 public class PaddleController : MonoBehaviour
 {
     [SerializeField] float _paddleSpeed = 5f;
+    [SerializeField] float _maxBounds = 7.5f;
+    [SerializeField] float _minBounds = -7.5f;
+
     float _movementHorizontal;
 
     void Update()
     {
         _movementHorizontal = Input.GetAxis("Horizontal");
-        transform.position += Vector3.right * _movementHorizontal * _paddleSpeed * Time.deltaTime;
+        if ((_movementHorizontal > 0 && transform.position.x < _maxBounds) || (_movementHorizontal < 0 && transform.position.x > _minBounds))
+        {
+            transform.position += Vector3.right * _movementHorizontal * _paddleSpeed * Time.deltaTime;
+        }
     }
 }
