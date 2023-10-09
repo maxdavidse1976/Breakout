255aab10b3108812f8878d79d5828480f52787f0
Max Davidse <m.davidse@rubicon.nl>
2023-10-09 10:15:08 +0200

    Added bounds to the ball and made the ball reset if it goes out through the bottom of the screen

--- Assets/_Project/Scripts/BallController.cs
+++ Assets/_Project/Scripts/BallController.cs
@@ -4,10 +4,33 @@ public class BallController : MonoBehaviour
 {
     [SerializeField] Rigidbody2D _rigidbody;
     [SerializeField] float _ballSpeed = 4f;
+    [SerializeField] float _bottomBounds = -4.8f;
+    [SerializeField] float _topBounds = 4.8f;
+    [SerializeField] float _leftBounds = -7.8f;
+    [SerializeField] float _rightBounds = -7.8f;
 
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        DropBall();
+    }
+
+    void Update()
+    {
+        if (transform.position.y < _bottomBounds)
+        {
+            ResetBall();
+        }
+    }
+
+    public void ResetBall()
+    {
+        transform.position = Vector3.zero;
+        DropBall();
+    }
+
+    void DropBall()
+    {
         _rigidbody.velocity = Vector2.down * _ballSpeed;
     }
 }
